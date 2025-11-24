3ad1c5ff4e8b2b2c6bc49269d8aa2f0be64f4f61
F4KHR111 <fakhriwirdiyan05@gmail.com>
2025-11-24 10:15:53 +0700

    Menambahkan middleware untuk parsing JSON dan mendefinisikan route API

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const connectDatabase = require('./config/db');
+const apiRoutes = require('./routes/api');
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
+app.use('/api', apiRoutes);
 
 async function startServer() {
     await connectDatabase();
